c2dd49c1d4b3c4b5ad44eb21416e13017b27dea7
josan_ishmeet_coding_assignment13 <ijosan@academic.rrc.ca>
2023-08-09 15:09:32 -0500

    Your commit message

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Makeup Products",
+    subtitle: "E-Commerce website",
+    description:
+      "Experience my makeup products project, a visually captivating e-commerce platform. Immerse yourself in a world of beauty through sleek design, seamless navigation, and comprehensive product information. Explore a user-friendly interface that showcases my skills in responsive web development and engaging user experiences.",
+    image: "./project-1.gif",
+    link: "https://github.com/ijosanRRC/Makeup_products.git",
+  },
+  {
+    title: "Beauty Glamour",
+    subtitle: "E-Commerce website",
+    description:
+    "Step into a realm of beauty and glamour with our captivating web project. Immerse yourself in a visually stunning showcase of makeup, skincare, and fashion trends. Elevating aesthetics and functionality, our platform redefines the art of elegance, offering a curated space for all things beauty and glamour.",
+    image: "./project-2.gif",
+    link: "https://github.com/ijosanRRC/Beauty_glamour.git",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
